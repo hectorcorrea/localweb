4dea33b7ebb1b3e452f024dded15d20ca5965cf8
Hector Correa <hector_correa@princeton.edu>
2026-02-16 16:42:11 -0500

    Added go.mod, updated script to produce mac universal exe

--- compile_all.sh
+++ compile_all.sh
@@ -1,4 +1,8 @@
 # Compile the executable for Mac, Linux,and Windows
-GOOS=darwin go build -o localweb
+GOOS=darwin GOARCH=arm64 go build -o localweb_arm64
+GOOS=darwin GOARCH=amd64 go build -o localweb_amd64
 GOOS=linux go build -o localweb_linux
 GOOS=windows GOARCH=386 go build -o localweb.exe
+
+# Create the universal Mac executable
+lipo -create -output localweb localweb_arm64 localweb_amd64
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hectorcorrea/localweb
+
+go 1.24.6
